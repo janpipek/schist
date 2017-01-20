3ef71b5cde3c1d6c7df54fb9cc17923b35978661
Jan Pipek <jan.pipek@gmail.com>
2017-01-20 11:50:05 +0100

    Enable conversion toDouble, toInt, etc.

--- src/cz/vzdusne/schist/Histogram.scala
+++ src/cz/vzdusne/schist/Histogram.scala
@@ -1,20 +1,35 @@
 package cz.vzdusne.schist
+import scala.reflect.{ClassTag, classTag}
 
 
-abstract class Histogram[T](frequencies : Array[T], binEdges : List[Array[Double]]) {
+abstract class Histogram[T : Numeric](frequencies : Array[T], binEdges : List[Array[Double]]) /* (implicit num: Numeric[T]) */ {
+  private val numerics = implicitly[Numeric[T]]
+
   protected var frequencies_ : Array[T] = frequencies
   protected var binEdges_ : List[Array[Double]] = binEdges
 
-  def frequenciesFlat : Seq[T] = frequencies_
+  protected def frequenciesFlat : Seq[T] = frequencies_
+  protected def frequenciesAsDouble : Array[Double] = frequencies_.map(x => numerics.toDouble(x))
+  protected def frequenciesAsInt : Array[Int] = frequencies_.map(x => numerics.toInt(x))
+  protected def frequenciesAsFloat : Array[Float] = frequencies_.map(x => numerics.toFloat(x))
+  protected def frequenciesAsLong : Array[Long] = frequencies_.map(x => numerics.toLong(x))
+
+  // def frequenciesAsInt : Array[Int] = frequencies_.map((x : T) => num.toInt(x))*/
   // def binEdges : Seq[Seq[U]] = binEdges_
 }
 
-class Histogram1D[T](frequencies : Array[T], binEdges : List[Array[Double]]) extends Histogram[T](frequencies, binEdges) {
+class Histogram1D[T : Numeric](frequencies : Array[T], binEdges : List[Array[Double]]) extends Histogram[T](frequencies, binEdges) {
   def this(frequencies : Array[T], binEdges : Array[Double]) = this(frequencies, List(binEdges))
+  def binEdges : Seq[Double] = binEdges_.head
+  def frequencies = frequencies_
+
+  def asDouble : Histogram1D[Double] = new Histogram1D[Double](frequenciesAsDouble, binEdges_)
 }
 
 object Histogram
 {
+
+
   private def make_edge_params(values : Array[Double], binCount : Int) = {
     val minEdge = values.min
     val maxEdge = values.max

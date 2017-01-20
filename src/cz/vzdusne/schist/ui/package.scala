b0d742e02dfa691f2757573534e810c2a43965d3
Jan Pipek <jan.pipek@gmail.com>
2017-01-20 11:51:20 +0100

    Try to implement plotting

---
+++ src/cz/vzdusne/schist/ui/package.scala
@@ -0,0 +1,28 @@
+package cz.vzdusne.schist
+
+import org.jfree.chart._
+import org.jfree.data.xy.DefaultXYDataset
+
+package object ui {
+  def plot(histogram: Histogram1D[Double]) : Unit = {
+    val dataset = new DefaultXYDataset
+    val x = histogram.binEdges.toArray
+    val y = histogram.asDouble.frequencies
+
+    dataset.addSeries("Title", Array(x, y))
+
+    val frame = new ChartFrame(
+      "Title",
+      ChartFactory.createScatterPlot(
+        "Plot",
+        "X Label",
+        "Y Label",
+        dataset,
+        org.jfree.chart.plot.PlotOrientation.HORIZONTAL,
+        false,false,false
+      )
+    )
+    frame.pack()
+    frame.setVisible(true)
+  }
+}
